e2d76a6bd502069fbcf98adaa0d3f77f4666f13e
teodoranemtanu <teodoranemtanu@gmail.com>
2025-02-18 19:00:25 +0200

    Make app-toolbar and fab-button fixed; add more data to test scrolling

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -9,6 +9,12 @@ export class InMemoryDataService {
   createDb() {
     const bookmarks: Bookmark[] = [
       {
+        id: '5baa7b2e-6da2-4501-88b3-2a1e847dfb80', name: 'StackOverflow', url: 'https://stackoverflow.com/', date: dayjs().subtract(1, 'day').toDate()
+      },
+      {
+        id: '8675709c-bd87-4249-99df-9b8cb61cd791', name: 'Vue', url: 'https://vue.dev/', date: dayjs().subtract(1, 'day').toDate()
+      },
+      {
         id: 'b82aafed-f23d-4c75-b45b-1744343e3575', name: 'React', url: 'https://react.dev/', date: dayjs().subtract(1, 'day').toDate()
       },
       {
@@ -16,8 +22,17 @@ export class InMemoryDataService {
 
       },
       {
-        id: '5baa7b2e-6da2-4501-88b3-2a1e847dfb80', name: 'Vue', url: 'https://vue.dev/', date: dayjs().subtract(2, 'day').toDate()
-      }
+        id: '4cd81fb6-c6ac-49fb-a8d9-8713051d2350', name: 'RxJs', url: 'https://rxjs.dev/guide/operators', date: dayjs().subtract(2, 'day').toDate()
+      },
+      {
+        id: '3cef7d64-0a3d-46d0-b3d1-a9e3f301e6a5', name: 'NgRx', url: 'https://ngrx.io/guide/store', date: dayjs().subtract(2, 'day').toDate()
+      },
+      {
+        id: 'c48ee34d-62e5-48a8-81ac-6d4b94146656', name: 'Angular Material', url: 'https://material.angular.io/', date: dayjs().subtract(3, 'day').toDate()
+      },
+      {
+        id: 'c48ee34d-62e5-48a8-81ac-6d4b94146656', name: 'MDN', url: 'https://developer.mozilla.org/en-US/', date: dayjs().subtract(4, 'day').toDate()
+      },
     ];
     return { bookmarks };
   }
